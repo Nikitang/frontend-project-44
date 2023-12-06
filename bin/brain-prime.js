4577a758942b9828947e8c919c256c35b292c727
Nikita <gerasimenya.05@mail.ru>
2023-12-06 16:09:55 +0300

    4-error correction

--- bin/brain-prime.js
+++ bin/brain-prime.js
@@ -4,7 +4,6 @@ import readlineSync from 'readline-sync';
 import consoleName from '../src/cli.js';
 
 function primeNumber(number) {
-
   for (let i = 2; i < number; i += 1) {
     if (number % i === 0) return false;
   }
@@ -21,7 +20,6 @@ function brainPrime() {
   console.log('Answer "yes" if given number is prime. Otherwise answer "no".');
 
   for (let g = 0; g < 3; g += 1) {
-
     const randomNum1 = Math.floor(Math.random() * (30 - 1 + 1)) + 1;
 
     console.log(`Question: ${randomNum1}`);
